c5c648e34c4aaef4b465c1a82af699197aff20c5
sagarshelake14 <sagarshelake0014@gmail.com>
2026-07-29 22:11:42 +0530

    env and appwrite configuration

---
+++ BlogPostSystem/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import conf from './conf/conf';
+
+function App() {
+
+  return (
+    <>
+     <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App
